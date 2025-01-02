5152bdb32cf7348b58736faf9ce149bf30b20b8d
pkozy <pkozyarchuk50@stuy.edu>
2025-01-02 18:59:57 -0500

    i can send stuff back and forth fine

--- basic_client.c
+++ basic_client.c
@@ -1,9 +1,17 @@
 #include "pipe_networking.h"
 
 int main() {
+	int bytes;
+	int to_server;
+	int from_server;
 
-  int to_server;
-  int from_server;
-
-  from_server = client_handshake( &to_server );
+	from_server = client_handshake( &to_server );
+	printf("handshake done\n");	
+	
+	while(1){
+		int recieved;
+		bytes = read(from_server, &recieved, 4);
+		if(bytes!=4)err();
+		printf("recieved %d!\n", recieved);
+	}
 }
--- basic_server.c
+++ basic_server.c
@@ -1,8 +1,19 @@
 #include "pipe_networking.h"
 
 int main() {
-  int to_client;
-  int from_client;
+	int to_client;
+	int from_client;
+	int bytes;
 
-  from_client = server_handshake( &to_client );
+	from_client = server_handshake( &to_client );
+	
+	printf("handshake done\n");
+	while(1){
+		srand(time(NULL));
+		int send = rand()%100 + 1;
+		bytes = write(to_client, &send, 4);
+		if(bytes!=4)err();
+		printf("sent %d!\n", send);
+		sleep(1);
+	}
 }
